e92db9355ec3d2982b85360e4dee581282917403
Devpe007 <anddev007pe@gmail.com>
2024-02-24 20:05:57 -0400

    creating the base function of picking up products from the first page of Amazon based on the search

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import { load } from 'cheerio';
+
+import fetchData from './services/axios';
 
 const app = express();
+
+app.get('/search', async (request, response) => {
+  try {
+    const { key } = request.query;
+
+    const content = await fetchData(`https://www.amazon.com.br/s?k=${key}`);
+    const $ = load(content);
+
+    let results = [];
+
+    $('div.s-result-item').each((index, element) => {
+      const title = $(element).find('h2').text();
+      const image = $(element).find('img.s-image').attr('src');
+      const stars = $(element).find('span.a-icon-alt').text();
+
+      results.push({
+        title,
+        image,
+        stars,
+      });
+
+    });
+
+    const filteredResults = results.filter(element => element.title !== '');
+
+    return response.json(filteredResults);
+  } catch (error) {
+      console.error('Error:', error);
+
+      return response.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.use(cors);
 
